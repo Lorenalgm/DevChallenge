da00fdf4347a3d20e27b26e0d255a2d2af82075b
Lorenalgm <lorena@proesc.com>
2020-05-19 22:42:33 -0300

    Created header component

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+export default function Header() {
+  return (
+      <header>
+        <div>Dev<span>Challenge</span></div>
+        <ul>
+          <li>Home</li>
+          <li>Challenges</li>
+        </ul>
+      </header>
+  )
+}
