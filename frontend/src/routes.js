a05885b4f16761a524700b0e4354969c4cc8a0f2
Lorenalgm <lorena@proesc.com>
2020-05-20 23:07:05 -0300

    Created challenge detail route

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Challenges from './pages/Challenges';
+import Challenge from './pages/Challenge';
 
 function Routes() {
     return(
@@ -10,6 +11,7 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/challenges" component={Challenges} />
+                <Route path="/challenges/:id" component={Challenge} />
             </Switch>
         </BrowserRouter>
     )
