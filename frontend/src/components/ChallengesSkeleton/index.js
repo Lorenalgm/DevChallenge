da21086aebf532ff96a2e6c3db144cbcee92524c
Lorenalgm <lorena@proesc.com>
2020-05-22 13:48:35 -0300

    Added skeleton in loading page

---
+++ frontend/src/components/ChallengesSkeleton/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Skeleton, {SkeletonTheme} from 'react-loading-skeleton';
+import './styles.css'
+
+export default function ChallengesSkeleton(){
+    return(
+        <SkeletonTheme color="#202020" highlightColor="#444">
+            <section className="challenges-skeleton">
+                <div className="challenge-skeleton">
+                    <Skeleton height={`100%`} width={`100%`} />
+                </div>
+                <div className="challenge-skeleton">
+                    <Skeleton height={`100%`} width={`100%`} />
+                </div>
+                <div className="challenge-skeleton">
+                    <Skeleton height={`100%`} width={`100%`} />
+                </div>
+                <div className="challenge-skeleton">
+                    <Skeleton height={`100%`} width={`100%`} />
+                </div>
+                <div className="challenge-skeleton">
+                    <Skeleton height={`100%`} width={`100%`} />
+                </div>
+                <div className="challenge-skeleton">
+                    <Skeleton height={`100%`} width={`100%`} />
+                </div>
+            </section>
+        </SkeletonTheme>
+    );
+}
--- frontend/src/pages/Challenges/index.js
+++ frontend/src/pages/Challenges/index.js
@@ -2,16 +2,20 @@ import React, { useEffect, useState } from 'react';
 import './styles.css';
 import api from '../../services/api';
 import Header from '../../components/Header';
+import ChallengesSkeleton from '../../components/ChallengesSkeleton';
 import { Link } from 'react-router-dom';
 
 export default function Challenges() {
   const [challenges, setChallenges] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadChallenges() {
       const response = await api.get('/challenges');
       console.log(response.data);
       setChallenges(response.data);
+
+      setLoading(false);
     }
 
     loadChallenges();
@@ -19,31 +23,32 @@ export default function Challenges() {
 
   return (
     <body>
-      <Header />
-      <section className="challenges">
-
-        {challenges.map(challenge => (
-          <div className="challenge-card" key={challenge._id}>
-            <Link to={{
-              pathname: `detail/${challenge._id}`,
-              challenge: challenge
-            }} >
-              <div className="card-image-container">
-                <img src={challenge.background} alt=""></img>
-              </div>
-            </Link>
-            <div className="card-content">
-              <h1 className="card-title">{challenge.name}</h1>
-              <p>{challenge.description}</p>
-            </div>
-            <div className="card-tech">
-              <p className="tech">{challenge.techs.join(', ')}</p>
-              <p className="level">{challenge.level}</p>
-            </div>
-          </div>
-        ))}
-
-      </section>
+      <Header />      
+        {loading && <ChallengesSkeleton />}
+        {!loading &&     
+        <section className="challenges">
+              {challenges.map(challenge => (
+                <div className="challenge-card" key={challenge._id}>
+                  <Link to={{
+                    pathname: `detail/${challenge._id}`,
+                    challenge: challenge
+                  }} >
+                    <div className="card-image-container">
+                      <img src={challenge.background} alt=""></img>
+                    </div>
+                  </Link>
+                  <div className="card-content">
+                    <h1 className="card-title">{challenge.name}</h1>
+                    <p>{challenge.description}</p>
+                  </div>
+                  <div className="card-tech">
+                    <p className="tech">{challenge.techs.join(', ')}</p>
+                    <p className="level">{challenge.level}</p>
+                  </div>
+                </div>
+              ))}
+        </section>
+        }
     </body>
   )
 }
