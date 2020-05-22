c2958c8a5b8648ef0277567b3cd57435ca90620c
Lorenalgm <lorena@proesc.com>
2020-05-21 22:47:23 -0300

    Created detail structure

--- frontend/src/pages/Detail/index.js
+++ frontend/src/pages/Detail/index.js
@@ -4,14 +4,27 @@ import Header from '../../components/Header';
 
 
 export default function Detail(props) {
-    
-    
-    return (
+  const challenge = props.location.challenge;
+  
+  return (
     <body>
       <Header />
-     <h1>teste1</h1>
-      <h1>{props.location.challenge.brief}</h1>
-     <h1>teste</h1>
+      <div>
+        <img src={challenge.background} alt="Challenge" />
+        <h1>{challenge.type}</h1>
+        <h1>{challenge.github_url}</h1>
+        <h1>{challenge.level}</h1>
+        <h1>{challenge.techs}</h1>
+        <h1>{challenge.images}</h1>
+      </div>
+      <div>
+        <h1>{challenge.name}</h1>
+        <h1>{challenge.description}</h1>
+        <h1>{challenge.brief}</h1>
+        <div>
+          <h1>{challenge.dev_id}</h1>
+        </div>
+      </div>
     </body>
   )
 }
