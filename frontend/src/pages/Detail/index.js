832a0831827bd10c619bc2d43c327f34c0a0587c
Lorenalgm <lorena@proesc.com>
2020-05-22 23:29:59 -0300

    Added styles

--- frontend/src/pages/Detail/index.js
+++ frontend/src/pages/Detail/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './styles.css';
 import Header from '../../components/Header';
 
-
 export default function Detail(props) {
   const challenge = props.location.challenge;
   
+
   return (
     <body>
       <Header />
       <div className="container">
         <div className="left-container">
           <img src={challenge.background} alt="Challenge" />
-          <button className="start-challenge" src={challenge.github_url}>Start challenge</button>
+          <a target="_blank" rel="noopener noreferrer" className="start-challenge" href={challenge.github_url}>Start challenge</a>
           <button className="more">More</button>
           {/* <div>{challenge.images}</div> */}
           <div className="stack">
@@ -25,9 +25,6 @@ export default function Detail(props) {
           <h1>{challenge.name}</h1>
           <h2>{challenge.description}</h2>
           <p>{challenge.brief}</p>
-          <div>
-            {/* <h1>{challenge.dev_id}</h1> */}
-          </div>
         </div>
       </div>
     </body>
