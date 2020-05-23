8b0905344d8ee64a594a844b573b2c717d72d4ae
Lorenalgm <lorena@proesc.com>
2020-05-23 16:39:37 -0300

    Configured dotenv

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: process.env.REACT_APP_API_URL
+    // baseURL: 'http://localhost:3333'
 });
 
 export default api;
