339032ec2808ee7f75b31ee09c7faaede2351a84
Lorenalgm <lorena@proesc.com>
2020-05-27 22:23:26 -0300

    Fixed detail css

--- frontend/src/pages/Detail/index.js
+++ frontend/src/pages/Detail/index.js
@@ -6,42 +6,43 @@ import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Detail(props) {
   const challenge = props.location.challenge;
-  
 
   return (
     <body>
       <Header />
       <div className="container">
         <div className="left-container">
+          <div className="stack">
+            <div className="challenge-techs">{challenge.techs}</div>
+            <div className="challenge-level">{challenge.level}</div>
+          </div>
           <img src={challenge.background} alt="Challenge" />
           <a target="_blank" rel="noopener noreferrer" className="start-challenge" href={challenge.github_url}>Start challenge</a>
           <button className="more">More</button>
           {/* <div>{challenge.images}</div> */}
-          <div className="stack">
-            <div>{challenge.level}</div>
-            <div>{challenge.techs}</div>
-            <div>{challenge.type}</div>
-          </div>
         </div>
         <div className="principal-container">
-          <h1>{challenge.name}</h1>
+          <div className="challenge">
+            <h1>{challenge.name}</h1>
+            <div className="challenge-type">{challenge.type}</div>
+          </div>
           <h2>{challenge.description}</h2>
           <p>{challenge.brief}</p>
           <div className="dev-container">
-          <img src={challenge.dev_id.avatar} alt="Dev" />
+            <img src={challenge.dev_id.avatar} alt="Dev" />
             <div className="dev-information">
               <span className="dev-name">{challenge.dev_id.name}</span>
               <span className="dev-position">{challenge.dev_id.position}</span>
             </div>
             <div className="dev-social-media">
-            {challenge.dev_id.github &&
-            <a className="icon" rel="noopener noreferrer" target="_blank" href={`https://github.com/${challenge.dev_id.github}`}>
-              <FontAwesomeIcon icon={faGithubSquare} />
-            </a>}
-            {challenge.dev_id.linkedin &&
-            <a className="icon" rel="noopener noreferrer" target="_blank" href={`https://www.linkedin.com/in/${challenge.dev_id.linkedin}`}>
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>}
+              {challenge.dev_id.github &&
+                <a className="icon" rel="noopener noreferrer" target="_blank" href={`https://github.com/${challenge.dev_id.github}`}>
+                  <FontAwesomeIcon icon={faGithubSquare} />
+                </a>}
+              {challenge.dev_id.linkedin &&
+                <a className="icon" rel="noopener noreferrer" target="_blank" href={`https://www.linkedin.com/in/${challenge.dev_id.linkedin}`}>
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>}
             </div>
           </div>
         </div>
