97834a3cde16f8fc7b4161c93ba8d6117667597f
Lorenalgm <lorena@proesc.com>
2020-05-28 21:17:02 -0300

    Added slider

--- frontend/src/pages/Detail/index.js
+++ frontend/src/pages/Detail/index.js
@@ -3,6 +3,9 @@ import './styles.css';
 import Header from '../../components/Header';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 
 export default function Detail(props) {
   const challenge = props.location.challenge;
@@ -15,19 +18,38 @@ export default function Detail(props) {
           <div className="stack">
             <div className="challenge-techs">{challenge.techs}</div>
             <div className="challenge-level">{challenge.level}</div>
+            <div className="challenge-type">{challenge.type}</div>
           </div>
-          <img src={challenge.background} alt="Challenge" />
-          <a target="_blank" rel="noopener noreferrer" className="start-challenge" href={challenge.github_url}>Start challenge</a>
-          <button className="more">More</button>
-          {/* <div>{challenge.images}</div> */}
+          <AwesomeSlider bullets={false} mobileTouch={true}>
+            <div data-src={challenge.background} />
+            {/* <div><img src={challenge.background} alt="Challenge" /></div> */}
+          </AwesomeSlider>
+          
+          <a target="_blank" rel="noopener noreferrer" className="start-challenge" href={challenge.github_url}>Iniciar desafio</a>
         </div>
         <div className="principal-container">
           <div className="challenge">
             <h1>{challenge.name}</h1>
-            <div className="challenge-type">{challenge.type}</div>
           </div>
-          <h2>{challenge.description}</h2>
-          <p>{challenge.brief}</p>
+            <h2>{challenge.description}</h2>
+
+            <div className="content">
+              <p>Seu desafio é {challenge.brief}.</p>
+              <div className="challenge-include">
+                <h3>O que está incluso?</h3>
+                <span><FontAwesomeIcon icon={faCheck} className="icon" />  Readme com instruções de requisitos, cores e fontes</span>
+                <span><FontAwesomeIcon icon={faCheck} className="icon" />  Imagens para adicionar no projeto</span>
+                <span><FontAwesomeIcon icon={faCheck} className="icon" />  Modelo com o design para utilizar como referência</span>
+                <span><FontAwesomeIcon icon={faCheck} className="icon" />  Arquivo contendo o texto que será utilizado</span>
+              </div>
+              <div className="challenge-start">
+                <h3>Como iniciar?</h3>
+                <span>1 - Clone o projeto com o código inicial</span>
+                <span>2 - Leia as instruções disponíveis no readme</span>
+                <span>3 - Inicie o desenvolvimento!</span>
+                <span>4 - Compartilhe seus resultados com a comunidade</span>
+              </div>
+            </div> 
           <div className="dev-container">
             <img src={challenge.dev_id.avatar} alt="Dev" />
             <div className="dev-information">
