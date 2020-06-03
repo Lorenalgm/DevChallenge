0591760b5f6867495da119310c1e131a2595a727
Edilson Junior <edilson.junior@kiper.com.br>
2020-06-03 08:58:52 -0300

    feat: add banner component

---
+++ frontend/src/components/Banner/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Banner() {
+    return (
+        <section className="banner">
+            <div className="typewriter">
+                <h1>Melhore suas habilidades</h1>
+            </div>
+            <h2>Com desafios de front-end, back-end e mobile</h2>
+            <Link to="/challenges">
+                <button>Bora codar!</button>
+            </Link>
+        </section>
+    );
+}
