daecd5862e29b18c4c982197cc85bf9a0831417e
mmouradev <matheus@setconsultoria.com.br>
2020-06-03 00:58:12 -0300

    refactor css page Challenges

--- frontend/src/pages/Challenges/index.js
+++ frontend/src/pages/Challenges/index.js
@@ -1,54 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import './styles.css';
-import api from '../../services/api';
-import Header from '../../components/Header';
-import ChallengesSkeleton from '../../components/ChallengesSkeleton';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
+import Header from "../../components/Header";
+import ChallengesSkeleton from "../../components/ChallengesSkeleton";
+import * as S from "./styled";
 
 export default function Challenges() {
-  const [challenges, setChallenges] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const [challenges, setChallenges] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function loadChallenges() {
-      const response = await api.get('/challenges');
-      // console.log(response.data);
-      setChallenges(response.data);
+    useEffect(() => {
+        async function loadChallenges() {
+            const response = await api.get("/challenges");
+            // console.log(response.data);
+            setChallenges(response.data);
 
-      setLoading(false);
-    }
-
-    loadChallenges();
-  }, []);
-
-  return (
-    <body>
-      <Header />      
-        {loading && <ChallengesSkeleton />}
-        {!loading &&     
-        <section className="challenges">
-              {challenges.map(challenge => (
-                <div className="challenge-card" key={challenge._id}>
-                  <Link className="link" to={{
-                    pathname: `detail/${challenge._id}`,
-                    challenge: challenge
-                  }} >
-                    <div className="card-image-container">
-                      <img src={challenge.background} alt=""></img>
-                    </div>
-                  <div className="card-content">
-                    <h1 className="card-title">{challenge.name}</h1>
-                    <p>{challenge.description}</p>
-                  </div>
-                  <div className="card-tech">
-                    <p className="tech">{challenge.techs.join(', ')}</p>
-                    <p className="level">{challenge.level}</p>
-                  </div>
-                  </Link>
-                </div>
-              ))}
-        </section>
+            setLoading(false);
         }
-    </body>
-  )
+
+        loadChallenges();
+    }, []);
+
+    return (
+        <body>
+            <Header />
+            {loading && <ChallengesSkeleton />}
+            {!loading && (
+                <S.Section>
+                    {challenges.map((challenge) => (
+                        <S.ChallengeCard key={challenge._id}>
+                            <S.Anchor
+                                to={{
+                                    pathname: `detail/${challenge._id}`,
+                                    challenge: challenge,
+                                }}
+                            >
+                                <S.CardImage>
+                                    <img src={challenge.background} alt="" />
+                                </S.CardImage>
+                                <S.CardContent>
+                                    <h1>{challenge.name}</h1>
+                                    <p>{challenge.description}</p>
+                                </S.CardContent>
+                                <S.CardTechs>
+                                    <p className="tech">
+                                        {challenge.techs.join(", ")}
+                                    </p>
+                                    <p className="level">{challenge.level}</p>
+                                </S.CardTechs>
+                            </S.Anchor>
+                        </S.ChallengeCard>
+                    ))}
+                </S.Section>
+            )}
+        </body>
+    );
 }
