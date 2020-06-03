8c18e5b33d52e25c72d02a601086946ef075368f
Vinicius S. de Castro <contatoviniciussc@gmail.com>
2020-06-03 14:15:49 -0300

    Add responsive view and animations on buttons.

--- frontend/src/components/Header/styled.js
+++ frontend/src/components/Header/styled.js
@@ -9,7 +9,8 @@ export const Header = styled.header`
     margin-right: 10%;
     margin-top: 2%;
 
-    @media (max-width: 800px) {
+
+    @media (max-width: 1200px) {
         display: flex;
         flex-direction: column;
         align-content: center;
@@ -19,40 +20,80 @@ export const Header = styled.header`
 `;
 
 export const Title = styled.div`
-    font-size: 30px;
+    font-size: 40px;
     font-weight: bold;
     color: #fff;
+    margin-top:0.5em;
+    transition: all .2s ease-out;
+       
+    :hover{
+        transition: all .2s ease-in-out;
+        transform: scale(1.2);
+    }
+
     span {
         color: #f4cf0a;
+    }
+
+    @media (max-width: 800px) {
+        margin-top: 2em;
     }
 `;
 
 export const Menu = styled.div`
+    
+    justify-content: center;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    text-align: center;
+
+
     ul {
         display: flex;
         flex-direction: row;
         list-style-type: none;
+        justify-items: center;
         font-size: 20px;
         color: #ffff;
 
-        li {
-            margin-left: 0;
+    li {
+        border-radius: 10%;
+        border: 2px solid white;
+        padding: 1em;
+        margin: 1em;
+        transition: all .2s ease-out;
         }
 
-        & li:not(:first-child) {
-            margin-left: 20px;
+
+    li:hover {
+        background:opacity: 0.8;
+        cursor: pointer;
+        transition: all .2s ease-in;
+        transform: scale(1.2);
+        }
+    
+    
+
+
+
+    @media screen and (max-width: 1200px) {
+        justify-content: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 3em;
+        
+        li {
+            width: 100vw;
+            margin: 1em;
+            border: none;
         }
 
         li:hover {
-            opacity: 0.8;
-            cursor: pointer;
+            background-color: #fff;
         }
-
-        @media (max-width: 800px) {
-            justify-content: center;
-            text-align: center;
-            align-content: center;
-            margin-top: 0.5em;
+            
         }
     }
 `;
@@ -60,6 +101,10 @@ export const Menu = styled.div`
 export const StyledLink = styled(NavLink)`
     text-decoration: none;
     color: #fff;
+
+    :hover{
+    color: #f4cf0a;
+    }
 
     &.is-active {
         color: #f4cf0a;
