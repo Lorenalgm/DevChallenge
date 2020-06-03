3c273421bcd5dd9df723b0c9f630b637d8a115bf
Edilson Junior <edilson.junior@kiper.com.br>
2020-06-03 10:45:19 -0300

    feat: get categories from api

--- frontend/src/components/CategoriesList/index.js
+++ frontend/src/components/CategoriesList/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
     faMobileAlt,
     faLaptopCode,
@@ -8,18 +8,27 @@ import {
     faShareSquare,
 } from "@fortawesome/free-solid-svg-icons";
 import CategoryListItem from "../CategoryListItem";
+import { getCategories } from "../../services/challenges.service";
 
-const categories = [
-    { title: "Front-end", icon: faFileCode },
-    { title: "Back-end", icon: faLaptopCode },
-    { title: "Mobile", icon: faMobileAlt },
-];
+// const categories = [
+//     { title: "Front-end", icon: faFileCode },
+//     { title: "Back-end", icon: faLaptopCode },
+//     { title: "Mobile", icon: faMobileAlt },
+// ];
 
 export default function CategoriesList() {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        async function loadCategories() {
+            const request = await getCategories();
+            setCategories(request);
+        }
+        loadCategories();
+    }, []);
     return (
         <section className="techs">
             {categories.map((category) => (
-                <CategoryListItem category={category} />
+                <CategoryListItem category={category} key={category._id} />
             ))}
         </section>
     );
