daa5f588c8068dcee4911dc5787da8354704d586
Luis Sousa <lv-ss@hotmail.com>
2020-08-12 04:14:37 -0300

    importando a Header localmente

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import * as S from './styled';
+import Header from '../../components/Header';
 
 export default function Login() {
-  return ( 
-    <S.Section>
-      <S.Form>asd</S.Form>
-    </S.Section>
-  );
+    return (
+        <>
+            <Header />
+            <S.Section>
+                <S.Form>asd</S.Form>
+            </S.Section>
+        </>
+    );
 }
