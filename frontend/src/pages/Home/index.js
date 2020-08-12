1e7695e994efb76a5c1385ede98fe3b2f28a72cd
Luis Sousa <lv-ss@hotmail.com>
2020-08-12 04:15:16 -0300

    importando Header localmente

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -3,10 +3,12 @@ import CategoriesList from '../../components/CategoriesList';
 import Newsletter from '../../components/Newsletter';
 import Steps from '../../components/Steps';
 import Banner from '../../components/Banner';
+import Header from '../../components/Header';
 
 export default function Home() {
     return (
         <>
+            <Header />
             <Banner />
             <CategoriesList />
             <Steps />
