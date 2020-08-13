f4ae1bbaeb31b8563bd18e716e02d947db1c91fc
Magdiel Dantas <magdielndantas@gmail.com>
2020-08-12 21:33:21 -0300

    importando header localmente

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -8,6 +8,7 @@ import Header from '../../components/Header';
 
 export default function Login() {
     return (
+        <Header />
         <S.Section>
             <S.Form>
                 <S.Title>Entrar</S.Title>
