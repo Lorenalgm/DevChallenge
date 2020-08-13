cc616743715d56ddb6d3a4de8a3d07e93f64d699
Magdiel Dantas <magdielndantas@gmail.com>
2020-08-12 21:34:57 -0300

    Update index.js

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -8,39 +8,41 @@ import Header from '../../components/Header';
 
 export default function Login() {
     return (
-        <Header />
-        <S.Section>
-            <S.Form>
-                <S.Title>Entrar</S.Title>
-                <S.Box>
-                    <S.Field>
-                        <FontAwesomeIcon icon={faAt} color="grey" />
-                        <S.Input placeholder="Email" type="email" required />
-                    </S.Field>
-                    <S.Field>
-                        <FontAwesomeIcon icon={faLock} color="grey" />
-                        <S.Input placeholder="Senha" type="password" required />
-                    </S.Field>
-                </S.Box>
-                <S.Buttons>
-                    <S.ButtonBox>
-                        <S.SocialButton>
-                            <FontAwesomeIcon icon={faGithub} />
-                        </S.SocialButton>
-                        <S.SocialButton>
-                            <FontAwesomeIcon icon={faGoogle} />
-                        </S.SocialButton>
-                    </S.ButtonBox>
+        <>
+            <Header />
+            <S.Section>
+                <S.Form>
+                    <S.Title>Entrar</S.Title>
+                    <S.Box>
+                        <S.Field>
+                            <FontAwesomeIcon icon={faAt} color="grey" />
+                            <S.Input placeholder="Email" type="email" required />
+                        </S.Field>
+                        <S.Field>
+                            <FontAwesomeIcon icon={faLock} color="grey" />
+                            <S.Input placeholder="Senha" type="password" required />
+                        </S.Field>
+                    </S.Box>
+                    <S.Buttons>
+                        <S.ButtonBox>
+                            <S.SocialButton>
+                                <FontAwesomeIcon icon={faGithub} />
+                            </S.SocialButton>
+                            <S.SocialButton>
+                                <FontAwesomeIcon icon={faGoogle} />
+                            </S.SocialButton>
+                        </S.ButtonBox>
 
-                    <S.ButtonBox>
-                        <S.Button className="cadastro">Criar conta</S.Button>
-                        <S.Button className="login">Entrar</S.Button>
-                    </S.ButtonBox>
-                </S.Buttons>
-                <S.Problem>
-                    Problemas?<strong> clique aqui</strong>
-                </S.Problem>
-            </S.Form>
-        </S.Section>
+                        <S.ButtonBox>
+                            <S.Button className="cadastro">Criar conta</S.Button>
+                            <S.Button className="login">Entrar</S.Button>
+                        </S.ButtonBox>
+                    </S.Buttons>
+                    <S.Problem>
+                        Problemas?<strong> clique aqui</strong>
+                    </S.Problem>
+                </S.Form>
+            </S.Section>
+        </>
     );
 }
