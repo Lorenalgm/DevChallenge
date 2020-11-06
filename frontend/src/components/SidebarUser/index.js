44e0a3b17263cc929b2f09d48a1d417552b15e45
Luis Sousa <lv-ss@hotmail.com>
2020-11-06 01:12:53 -0300

    :construction:  resolvendo quebras de desing e responsividade WIP

--- frontend/src/components/SidebarUser/index.js
+++ frontend/src/components/SidebarUser/index.js
@@ -1,52 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import Icon from 'react-web-vector-icons';
-import { Aside, Img, Ul, Li, UserInfo } from './styles';
+import { Aside, Img, UserInfo, StyledLink, Menu, MenuMobile } from './styles';
 
 function SidebarUser() {
+    const location = useLocation();
+    const [clicked, setClicked] = useState(false);
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        setClicked(false);
+        setUser({
+            name: 'Dev Challenge',
+            username: '@devChallenge',
+            img: '',
+        });
+    }, [location]);
+
     return (
-        <Aside>
-            <UserInfo>
-                <Img
-                    src="https://avatars2.githubusercontent.com/u/48053804?s=460&u=41a09241f1f585f000c86e67bf3b6147aed66ed6&v=4"
-                    alt=""
-                />
-                <p className="user-name">Luis Sousa</p>
-                <span>@OIncognita</span>
-            </UserInfo>
-            <Ul>
-                <Li>
-                    <Icon
-                        style={{ marginRight: 20 }}
-                        name="desktop"
-                        color="#f4cf0a"
-                        size={20}
-                        font="FontAwesome"
+        <>
+            <MenuMobile onClick={() => setClicked(!clicked)}>
+                {!clicked ? (
+                    <FontAwesomeIcon icon={faBars} />
+                ) : (
+                    <FontAwesomeIcon icon={faTimes} />
+                )}
+            </MenuMobile>
+            <Aside open={!clicked}>
+                <UserInfo>
+                    <Img
+                        src={
+                            user.img
+                                ? user.img
+                                : 'https://trello-attachments.s3.amazonaws.com/590fa896d2d25e50583de620/500x500/0bdcc819ea145cb0167619c6d00f2174/D.png'
+                        }
+                        alt=""
                     />
-                    Desafios
-                </Li>
-                <Li>
-                    <Icon
-                        style={{ marginRight: 20 }}
-                        name="code"
-                        color="#f4cf0a"
-                        size={20}
-                        font="FontAwesome"
-                    />
-                    Meus Desafios
-                </Li>
-                <Li>
-                    <Icon
-                        style={{ marginRight: 20 }}
-                        name="desktop"
-                        color="#f4cf0a"
-                        size={20}
-                        font="FontAwesome"
-                    />
-                    Comunidade
-                </Li>
-            </Ul>
-        </Aside>
+                    <p className="user-name">{user.name}</p>
+                    <span>{user.username}</span>
+                </UserInfo>
+
+                <Menu>
+                    <ul>
+                        <li>
+                            <StyledLink
+                                activeClassName="is-active"
+                                to="/challenges"
+                            >
+                                <Icon
+                                    style={{ marginRight: 20 }}
+                                    name="desktop"
+                                    color="#f4cf0a"
+                                    size={20}
+                                    font="FontAwesome"
+                                />
+                                Desafios
+                            </StyledLink>
+                        </li>
+                        <li>
+                            <StyledLink
+                                activeClassName="is-active"
+                                to="/dashboard/myChallenges"
+                            >
+                                <Icon
+                                    style={{ marginRight: 20 }}
+                                    name="code"
+                                    color="#f4cf0a"
+                                    size={20}
+                                    font="FontAwesome"
+                                />
+                                Meus Desafios
+                            </StyledLink>
+                        </li>
+                        <li>
+                            <StyledLink activeClassName="is-active" to="/devs">
+                                <Icon
+                                    style={{ marginRight: 20 }}
+                                    name="desktop"
+                                    color="#f4cf0a"
+                                    size={20}
+                                    font="FontAwesome"
+                                />
+                                Comunidade
+                            </StyledLink>
+                        </li>
+                    </ul>
+                </Menu>
+            </Aside>
+        </>
     );
 }
 
--- frontend/src/components/SidebarUser/styles.js
+++ frontend/src/components/SidebarUser/styles.js
@@ -1,28 +1,31 @@
 import styled from 'styled-components';
+import media from 'styled-media-query';
+import { NavLink } from 'react-router-dom';
 
 export const Aside = styled.aside`
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
     display: flex;
     align-items: center;
     flex-direction: column;
     text-align: center;
-    z-index: -1;
 
     width: 350px;
-    height: 100%;
     background: var(--secondary);
     color: var(--white);
+
+    ${media.between('medium', 'large')`
+        width: 278px;
+    `}
+
+    ${media.lessThan('860px')`
+        display: none; 
+    `}
 `;
 
 export const UserInfo = styled.div`
-    margin-top: 50px;
+    margin-top: 40px;
 
     & > p {
-        font-size: 20px;
-        font-weight: bold;
+        font-size: 16px;
         font-family: Roboto;
     }
 
@@ -33,26 +36,65 @@ export const UserInfo = styled.div`
 `;
 
 export const Img = styled.img`
-    height: 120px;
-    width: 120px;
+    height: 90px;
+    width: 90px;
     border-radius: 50%;
     margin-bottom: 10px;
-    border: 4px solid var(--yellow);
+    border: 3px solid var(--yellow);
 `;
 
-export const Ul = styled.ul`
-    width: 100%;
-    margin-top: 30px;
-    list-style: none;
-    text-align: center;
+export const Menu = styled.div`
+    ul {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 350px;
+        margin-top: 60px;
+        list-style: none;
+
+        ${media.between('medium', 'large')`
+        width: 278px;
+    `}
+
+        li {
+            width: 200px;
+            height: 45px;
+            display: flex;
+            align-items: center;
+            font-size: 18px;
+        }
+
+        li:hover {
+            opacity: 0.8;
+        }
+    }
 `;
 
-export const Li = styled.li`
-    width: 100%;
-    height: 50px;
-    display: flex;
-    padding: 0 50px;
-    text-align: center;
-    align-items: center;
-    font-size: 20px;
+export const StyledLink = styled(NavLink)`
+    text-decoration: none;
+    color: var(--white);
+
+    &.is-active {
+        color: var(--yellow);
+    }
+`;
+
+export const MenuMobile = styled.div`
+    display: none;
+
+    transition: 0.25s;
+
+    ${media.lessThan('medium')`
+        display: block;
+        position: absolute;
+        font-size: 25px;
+        color: var(--white);
+        top: 50px;
+        left: 20px;
+        margin-right: 20px;
+        cursor: pointer;
+
+        z-index: 9999;
+    `}
 `;
