7c2ac2176b7ff141d504462c9dee2ec9bdfe55da
Luis Sousa <lv-ss@hotmail.com>
2020-11-07 22:25:23 -0300

    :construction: Adicionando filtros a pagina de challenges WIP

--- frontend/src/pages/Challenges/index.js
+++ frontend/src/pages/Challenges/index.js
@@ -5,34 +5,95 @@ import ChallengeCard from '../../components/ChallengeCard';
 import Header from '../../components/Header';
 import * as S from './styled';
 
+const languages = [
+    { id: 1, name: 'React Native' },
+    { id: 2, name: 'Free Choice' },
+    { id: 3, name: 'Javascript' },
+    { id: 4, name: 'HTML' },
+    { id: 5, name: 'CSS' },
+];
+
+const types = [
+    { id: 1, name: 'Frontend' },
+    { id: 2, name: 'Backend' },
+    { id: 3, name: 'Mobile' },
+];
+
 export default function Challenges({ location }) {
     const [challenges, setChallenges] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [languageFilter, setLanguageFilter] = useState('');
+    const [typeFilter, setTypeFilter] = useState(location.search.split('=')[1]);
+
     function capitalize(s) {
         return s && s[0].toUpperCase() + s.slice(1);
     }
+
     useEffect(() => {
         window.scrollTo(0, 0);
         async function loadChallenges() {
             let response = [];
-            let type = location.search.split('=')[1];
-            type = capitalize(type);
-            if (type) {
+            if (typeFilter) {
+                const type = capitalize(typeFilter);
                 response = await api.get(`/challenges/?type=${type}`);
             } else {
                 response = await api.get('/challenges');
             }
-            // console.log(response.data);
             setChallenges(response.data);
 
             setLoading(false);
         }
 
         loadChallenges();
-    }, [location]);
+    }, [location, typeFilter, languageFilter]);
+
     return (
         <>
             <Header />
+            <S.Head>
+                <h1>Desafios</h1>
+                <S.Filters>
+                    <S.InputGroup>
+                        <label htmlFor="categoria">Categoria</label>
+                        <S.Select>
+                            <select
+                                name="categoria"
+                                onChange={(e) => {
+                                    setTypeFilter(e.target.value);
+                                }}
+                                defaultValue={capitalize(typeFilter)}
+                            >
+                                <option value="">All</option>
+                                {types.map((type) => (
+                                    <option key={type.id} value={type.nam}>
+                                        {type.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </S.Select>
+                    </S.InputGroup>
+                    <S.InputGroup>
+                        <label htmlFor="linguagem">Linguagem</label>
+                        <S.Select>
+                            <select
+                                name="linguagem"
+                                id=""
+                                onChange={(e) =>
+                                    setLanguageFilter(e.target.value)
+                                }
+                            >
+                                <option value="">All</option>
+                                {languages.map((lang) => (
+                                    <option key={lang.id} value={lang.name}>
+                                        {lang.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </S.Select>
+                    </S.InputGroup>
+                </S.Filters>
+            </S.Head>
+
             {loading && <ChallengesSkeleton cards={6} />}
             {!loading && (
                 <S.Section>
@@ -42,6 +103,7 @@ export default function Challenges({ location }) {
                                 challenge={challenge}
                                 buttonText="ver detalhes"
                                 redirect={`/challenges/${challenge._id}/details`}
+                                key={challenge._id}
                             />
                         );
                     })}
