8a90971a7ebbce3a9bd2058aad8a2b50e5ed29eb
leonardo <leolub150@gmail.com>
2021-02-11 11:11:01 -0300

    tests: ensure loadChallenges gets all the filtered challenges when have an filter

--- frontend/__tests__/services/loadChallenges.spec.js
+++ frontend/__tests__/services/loadChallenges.spec.js
@@ -1,19 +1,25 @@
 import MockAdapter from 'axios-mock-adapter';
 import loadChallenges from '../../src/services/loadChallenges';
 import api from '../../src/services/api';
+import capitalize from '../../src/utils/capitalize';
 
+jest.mock('../../src/utils/capitalize');
 const apiMock = new MockAdapter(api, { delayResponse: 1000 });
 
 describe(loadChallenges, () => {
     const mock = {
-        filter: { undefined },
-        response: { data: { with_no_filters: [] } },
+        filter: { undefined, any: 'any-filter' },
+        response: { with_no_filters: [], with_filters: [{}, {}] },
     };
 
     beforeEach(() => {
+        capitalize.mockImplementation((word) => word);
+
+        apiMock.onGet('challenges').reply(200, mock.response.with_no_filters);
+
         apiMock
-            .onGet('challenges')
-            .reply(200, mock.response.data.with_no_filters);
+            .onGet(`challenges/?type=${mock.filter.any}`)
+            .reply(200, mock.response.with_filters);
     });
 
     describe('when have no filter', () => {
@@ -21,7 +27,16 @@ describe(loadChallenges, () => {
             const { challenges } = await loadChallenges({
                 typeFilter: mock.filter.undefined,
             });
-            expect(challenges).toEqual(mock.response.data.with_no_filters);
+            expect(challenges).toEqual(mock.response.with_no_filters);
+        });
+    });
+
+    describe('when have filter', () => {
+        it('should get all the filtered challenges', async () => {
+            const { challenges } = await loadChallenges({
+                typeFilter: mock.filter.any,
+            });
+            expect(challenges).toEqual(mock.response.with_filters);
         });
     });
 });
