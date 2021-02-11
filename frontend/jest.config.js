38cbde58de03e655e881ed9261b154599c7eae77
leonardo <leolub150@gmail.com>
2021-02-11 16:10:52 -0300

    test: ensure Detail page calls api and render the data

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     modulePathIgnorePatterns: ['__utils__'],
     setupFilesAfterEnv: ['<rootDir>/__tests__/__utils__/setup-tests.js'],
+    moduleNameMapper: {
+        '\\.(css|less)$': 'identity-obj-proxy',
+    },
 };
