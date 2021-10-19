04fe6ef84ae675fd682723325e22c589f63fb4c4
Luis Sousa <lv-ss@hotmail.com>
2021-10-19 09:52:39 -0300

    fix env key and api reference

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://devchallengeapi2.herokuapp.com/',
-    // baseURL: 'http://localhost:3333',
+    baseURL: process.env.REACT_APP_DEVCHALLENGE_API,
 });
 
 export default api;
