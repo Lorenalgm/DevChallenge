593023002acb62cd5e25f76fe7c0b17b65b61f2a
Filipe Batista <filipe.batista@externos-br.cencosud.com>
2023-09-23 16:04:52 -0300

    limpando o projeto antigo

--- frontend/src/components/CategoriesList/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Techs = styled.section`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-`;
--- frontend/src/services/challenges.service.js
+++
@@ -1,4 +0,0 @@
-import api from './api';
-
-export const getCategories = () =>
-    api.get('categories').then(({ data }) => data);
