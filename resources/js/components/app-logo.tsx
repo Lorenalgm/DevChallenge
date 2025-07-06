66735cb9a5a355da392ef1f80fb3cbd465a1a998
Millena Limaverde <millenalimaverde@outlook.com>
2025-07-06 02:07:48 -0300

    refactor: 💡 initial setup with Laravel and Inertia

---
+++ resources/js/components/app-logo.tsx
@@ -0,0 +1,14 @@
+import AppLogoIcon from './app-logo-icon';
+
+export default function AppLogo() {
+    return (
+        <>
+            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
+                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            </div>
+            <div className="ml-1 grid flex-1 text-left text-sm">
+                <span className="mb-0.5 truncate leading-tight font-semibold">Laravel Starter Kit</span>
+            </div>
+        </>
+    );
+}
